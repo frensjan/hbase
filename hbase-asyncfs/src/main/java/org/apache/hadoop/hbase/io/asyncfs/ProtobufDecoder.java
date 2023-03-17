6c037587c6eb60222a0e29e6762ee5e659c41c27
Frens Jan Rumph <mail@frensjan.nl>
2023-03-17 14:03:29 +0100

    disable thirdparty protobuf for now

--- hbase-asyncfs/src/main/java/org/apache/hadoop/hbase/io/asyncfs/ProtobufDecoder.java
+++ hbase-asyncfs/src/main/java/org/apache/hadoop/hbase/io/asyncfs/ProtobufDecoder.java
@@ -114,14 +114,14 @@ public class ProtobufDecoder extends MessageToMessageDecoder<ByteBuf> {
     protobufMessageLiteClass = com.google.protobuf.MessageLite.class;
     protobufMessageLiteBuilderClass = com.google.protobuf.MessageLite.Builder.class;
 
-    try {
-      protobufMessageLiteClass = Class.forName("org.apache.hadoop.thirdparty.protobuf.MessageLite");
-      protobufMessageLiteBuilderClass =
-        Class.forName("org.apache.hadoop.thirdparty.protobuf.MessageLite$Builder");
-      LOG.debug("Hadoop 3.3 and above shades protobuf.");
-    } catch (ClassNotFoundException e) {
-      LOG.debug("Hadoop 3.2 and below use unshaded protobuf.", e);
-    }
+//    try {
+//      protobufMessageLiteClass = Class.forName("org.apache.hadoop.thirdparty.protobuf.MessageLite");
+//      protobufMessageLiteBuilderClass =
+//        Class.forName("org.apache.hadoop.thirdparty.protobuf.MessageLite$Builder");
+//      LOG.debug("Hadoop 3.3 and above shades protobuf.");
+//    } catch (ClassNotFoundException e) {
+//      LOG.debug("Hadoop 3.2 and below use unshaded protobuf.", e);
+//    }
 
     try {
       getParserForTypeMethod = protobufMessageLiteClass.getDeclaredMethod("getParserForType");
